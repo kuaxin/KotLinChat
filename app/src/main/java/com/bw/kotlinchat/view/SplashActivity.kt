652b36a835e9ad1c2249106b8e7a0e56e00ebcca
rgx <928990329@qq.com>
2019-04-11 13:54:47 +0800

    登录页面创建

--- app/src/main/java/com/bw/kotlinchat/view/SplashActivity.kt
+++ app/src/main/java/com/bw/kotlinchat/view/SplashActivity.kt
@@ -10,6 +10,8 @@ import com.bw.kotlinchat.LoginActivity
 import com.bw.kotlinchat.MainActivity
 import com.bw.kotlinchat.R
 import com.bw.kotlinchat.presenter.SplashPresenterImpl
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class SplashActivity : BaseActivity(),SplashView {
     override fun setLayout(): Int {
@@ -29,11 +31,13 @@ class SplashActivity : BaseActivity(),SplashView {
 
     override fun checkLogin(isLogined: Boolean) {
         if(isLogined){
+
+
 //            var intent:Intent = Intent(this,MainActivity::class.java)
 //            startActivity(intent)
             startActivity1(MainActivity().javaClass,true);
         }else{
-            showToast("你好")
+            toast("你好")
             var objectanimator: ObjectAnimator = ObjectAnimator.ofFloat(iv,"alpha",0f,1f);
             objectanimator.setDuration(2000).start();
             mhandler.postDelayed(Runnable {
